4248249b7ea742ee5dd25d9a17e2b089ac6dffee
hpalazzi <hugopalazzi75@gmail.com>
2024-10-24 20:33:09 +0200

    styles come from files

---
+++ frontend/src/routes/(private)/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+        import '@tcf/styles/styles.scss';
+
+</script>
+
+<div class="wrapper">
+	<div class="wrapper__body">
+		<main role="main" id="contenu-site">
+			<slot />
+		</main>
+	</div>
+</div>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-
+	css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `@use '@tcf/styles/utilities/mixins' as *;`,
+            },
+        },
+    },
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
