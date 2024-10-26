d2d5c067e52bb6e555f365907070f847c63d2632
hpalazzi <hugopalazzi75@gmail.com>
2024-10-26 10:08:43 +0200

    ad style to the page

--- frontend/src/routes/(private)/+page.svelte
+++ frontend/src/routes/(private)/+page.svelte
@@ -1,2 +1,106 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { createSelect, melt } from '@melt-ui/svelte';
+	import { Check, ChevronDown, ChevronUp, Icon } from 'svelte-hero-icons';
+	import { fade } from 'svelte/transition';
+
+	const options = {
+		sweet: ['Caramel', 'Chocolate', 'Strawberry', 'Cookies & Cream'],
+		savory: ['Basil', 'Bacon', 'Rosemary']
+	};
+
+	const {
+		elements: { trigger, menu, option, group, groupLabel, label },
+		states: { selectedLabel, open },
+		helpers: { isSelected }
+	} = createSelect<string>({
+		forceVisible: true,
+		positioning: {
+			placement: 'bottom',
+			fitViewport: true,
+			sameWidth: true
+		}
+	});
+</script>
+
+<p>{$open}</p>
+<div class="background-image">
+	<div class="form-container">
+		<div class="flex flex-col gap-1">
+			<label class="block text-magnum-900" use:melt={$label}>Favorite Flavor</label>
+			<button
+				class="flex h-10 min-w-[220px] items-center justify-between rounded-lg bg-white px-3 py-2
+            text-magnum-700 shadow transition-opacity hover:opacity-90"
+				use:melt={$trigger}
+				aria-label="Food"
+			>
+				{$selectedLabel || 'Select a flavor'}
+				<Icon src={ChevronDown} size="18" />
+			</button>
+			{#if $open}
+				<div
+					class=" z-10 flex max-h-[300px] flex-col
+              overflow-y-auto rounded-lg bg-white p-1
+              shadow focus:!ring-0"
+					use:melt={$menu}
+					transition:fade={{ duration: 150 }}
+				>
+					{#each Object.entries(options) as [key, arr]}
+						<div use:melt={$group(key)}>
+							<div
+								class="py-1 pl-4 pr-4 font-semibold capitalize text-neutral-800"
+								use:melt={$groupLabel(key)}
+							>
+								{key}
+							</div>
+							{#each arr as item}
+								<div
+									class="relative cursor-pointer rounded-lg py-1 pl-8 pr-4 text-neutral-800
+                        hover:bg-magnum-100 focus:z-10
+                        focus:text-magnum-700
+                        data-[highlighted]:bg-magnum-200 data-[highlighted]:text-magnum-900
+                        data-[disabled]:opacity-50"
+									use:melt={$option({ value: item, label: item })}
+								>
+									<div class="check {$isSelected(item) ? 'block' : 'hidden'}">
+										<Icon src={Check} size="18" />
+									</div>
+
+									{item}
+								</div>
+							{/each}
+						</div>
+					{/each}
+				</div>
+			{/if}
+		</div>
+	</div>
+</div>
+
+<style lang="scss">
+	.background-image {
+		background-image: url('/assets/images/tcf_background_no_bg.png');
+		background-size: cover;
+		background-position: center;
+		background-repeat: no-repeat;
+		height: 100vh;
+
+		.form-container {
+			border: 1px solid white;
+		}
+	}
+
+	.multiselect-label {
+		display: block;
+	}
+	.multiselect-button {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		border-radius: 0.5rem;
+		background-color: #ffffff;
+	}
+
+	.conteneur {
+		width: 100%;
+	}
+</style>
