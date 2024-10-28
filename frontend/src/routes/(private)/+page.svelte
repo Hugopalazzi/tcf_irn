4dc736aa9d322aaee55e974ff7ae049ae0e3c1e0
hpalazzi <hugopalazzi75@gmail.com>
2024-10-28 15:59:10 +0100

    add chevron down/Up correct style

--- frontend/src/routes/(private)/+page.svelte
+++ frontend/src/routes/(private)/+page.svelte
@@ -3,78 +3,62 @@
 	import { Check, ChevronDown, ChevronUp, Icon } from 'svelte-hero-icons';
 	import { fade } from 'svelte/transition';
 
-	const options = {
-		sweet: ['Caramel', 'Chocolate', 'Strawberry', 'Cookies & Cream'],
-		savory: ['Basil', 'Bacon', 'Rosemary']
-	};
+	interface MultiSelectOption {
+		value: string;
+		label: string;
+	}
+
+	const meltOptions: MultiSelectOption[] = [
+		{ label: 'English', value: 'eng' },
+		{ label: 'Français', value: 'fr' }
+	];
 
 	const {
-		elements: { trigger, menu, option, group, groupLabel, label },
-		states: { selectedLabel, open },
-		helpers: { isSelected }
+		elements: { trigger, menu, option },
+		states: { selectedLabel, open, selected }
 	} = createSelect<string>({
 		forceVisible: true,
 		positioning: {
 			placement: 'bottom',
 			fitViewport: true,
 			sameWidth: true
-		}
+		},
+		defaultSelected: { label: 'English', value: 'eng' }
 	});
 </script>
 
 <div class="page-container">
 	<div class="left-size">
 		<img class="logo" src="/assets/images/logo_my_story.png" alt="My story" />
-		<h2>Aprenez le francais en vous amusant avec Mystory.</h2>
+		<h2>Apprenez le francais en vous amusant avec Mystory.</h2>
 	</div>
 	<div class="right-size">
-		<div class="flex flex-col gap-1">
-			<button
-				class="flex h-10 min-w-[220px] items-center justify-between rounded-lg bg-white px-3 py-2
-            text-magnum-700 shadow transition-opacity hover:opacity-90"
-				use:melt={$trigger}
-				aria-label="Food"
-			>
-				{$selectedLabel || 'Select a flavor'}
-				<Icon src={ChevronDown} size="18" />
-			</button>
+		<button class="select-button" use:melt={$trigger} aria-label="language">
+			<span class="selected-language">
+				{$selected?.label}
+			</span>
 			{#if $open}
-				<div
-					class=" z-10 flex max-h-[300px] flex-col
-              overflow-y-auto rounded-lg bg-white p-1
-              shadow focus:!ring-0"
-					use:melt={$menu}
-					transition:fade={{ duration: 150 }}
-				>
-					{#each Object.entries(options) as [key, arr]}
-						<div use:melt={$group(key)}>
-							<div
-								class="py-1 pl-4 pr-4 font-semibold capitalize text-neutral-800"
-								use:melt={$groupLabel(key)}
-							>
-								{key}
-							</div>
-							{#each arr as item}
-								<div
-									class="relative cursor-pointer rounded-lg py-1 pl-8 pr-4 text-neutral-800
-                        hover:bg-magnum-100 focus:z-10
-                        focus:text-magnum-700
-                        data-[highlighted]:bg-magnum-200 data-[highlighted]:text-magnum-900
-                        data-[disabled]:opacity-50"
-									use:melt={$option({ value: item, label: item })}
-								>
-									<div class="check {$isSelected(item) ? 'block' : 'hidden'}">
-										<Icon src={Check} size="18" />
-									</div>
-
-									{item}
-								</div>
-							{/each}
-						</div>
-					{/each}
-				</div>
+				<Icon src={ChevronUp} size="18" />
+			{:else}
+				<Icon src={ChevronDown} size="18" />
 			{/if}
-		</div>
+		</button>
+		{#if $open}
+			<div class="options-list-wrapper" use:melt={$menu}>
+				{#each meltOptions as item}
+					<div
+						class="multi-select-option checkbox"
+						use:melt={$option({ value: item.value, label: item.label })}
+					>
+						<button class="select-option-text">
+							<p class="select-text">
+								{item.label}
+							</p>
+						</button>
+					</div>
+				{/each}
+			</div>
+		{/if}
 	</div>
 </div>
 
@@ -94,8 +78,45 @@
 		}
 
 		.right-size {
-			border: rem(1) solid white;
+			border: rem(1) solid var(--dark-purple);
 			border-radius: rem(16);
+			padding: rem(20);
+			background-image: linear-gradient(to bottom right, var(--dark-purple), var(--light-purple));
+
+			.select-button {
+				display: flex;
+				min-width: 13.75rem;
+				padding-right: 0.75rem;
+				justify-content: space-between;
+				align-items: center;
+				border-radius: 0.5rem;
+				height: 2.5rem;
+				background-color: #ffffff;
+				transition-property: opacity;
+				transition-duration: 300ms;
+				box-shadow:
+					0 1px 3px 0 rgba(0, 0, 0, 0.1),
+					0 1px 2px 0 rgba(0, 0, 0, 0.06);
+
+				.selected-language {
+					max-width: rem(400);
+					display: -webkit-box;
+					-webkit-box-orient: vertical;
+					-webkit-line-clamp: 1;
+					line-clamp: 1;
+					overflow: hidden;
+					text-align: left;
+				}
+			}
 		}
+	}
+
+	.options-list-wrapper {
+		background-color: white;
+		z-index: 1;
+		box-shadow:
+			0 rem(1) rem(3) 0 rgb(0 0 0 / 0.1),
+			0 rem(1) rem(2) rem(-1) rgb(0 0 0 / 0.1);
+		border-radius: rem(8);
 	}
 </style>
