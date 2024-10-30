3470d16a62b9921fb2f67bdaef963b435ea3f1d7
hpalazzi <hugopalazzi75@gmail.com>
2024-10-30 15:19:10 +0100

    create every input

--- frontend/src/routes/(public)/inscription/+page.svelte
+++ frontend/src/routes/(public)/inscription/+page.svelte
@@ -8,14 +8,32 @@
 		<h2>Apprenez le francais en vous amusant avec Mystory.</h2>
 	</div>
 	<div class="right-size">
-	<h1>Register</h1>
+		<h1 class ='title'>Register</h1>
+
+		<div class="input-wrapper">
+			<label for="email"> Your email </label>
+			<input id="email" name="email" placeholder="john.doe@mail.com" />
+		</div>
+		<div class="input-wrapper">
+			<label for="username"> Your username </label>
+			<input id="username" name="username" placeholder="johndoe" />
+		</div>
+		<div class="input-wrapper">
+			<label for="password"> Set password </label>
+			<input type="password" id="password" name="password" placeholder="New password" />
+		</div>
+		<div class="input-wrapper">
+			<label for="password-confirmation"> Password confirmation </label>
+			<input
+				type="password"
+				id="password-confirmation"
+				name="password-confirmation"
+				placeholder="Password confirmation"
+			/>
+		</div>
 
 		<button class="btn btn-primary btn-icon">
-			<Icon src={Envelope} size="18" />Create an account
+			<Icon src={Envelope} size="18" />Sign Up
 		</button>
-
-		<div class="two-button">
-			<button class="btn btn-secondary"> <span>Login</span></button>
-		</div>
 	</div>
 </div>
