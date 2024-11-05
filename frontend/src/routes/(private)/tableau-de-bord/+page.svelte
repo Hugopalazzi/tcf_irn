f1be1f0236af83fcc3d6dedf986744cf631c78ee
hpalazzi <hugopalazzi75@gmail.com>
2024-11-05 13:25:14 +0100

    Add chart js bar graph

--- frontend/src/routes/(private)/tableau-de-bord/+page.svelte
+++ frontend/src/routes/(private)/tableau-de-bord/+page.svelte
@@ -1,8 +1,31 @@
+<script lang="ts">
+	import Bar from './Bar.svelte';
+
+	const mockBar: { libelle: string; valeur: string[] }[] = [
+		{
+			libelle: 'Dates',
+			valeur: ['M', 'T', 'W', 'T', 'F', 'S', 'S']
+		},
+		{
+			libelle: 'LC',
+			valeur: ['1', '2', '42', '10', '22', '2', '2']
+		},
+		{
+			libelle: 'OC',
+			valeur: ['25', '22', '12', '14', '10', '2', '0']
+		},
+		{
+			libelle: 'WE',
+			valeur: ['4', '2', '1', '7', '0', '22', '15']
+		}
+	];
+</script>
+
 <h1>Dashboard</h1>
 <h3>Hello, Aryan 👋​</h3>
 <div class="centered-content">
-	<span class="graph-title"> </span>
-	Tests Completed In Past 7 Days
+	<span class="graph-title">Tests Completed In Past 7 Days </span>
+	<Bar dataSet={mockBar} />
 </div>
 
 <style lang="scss">
@@ -14,7 +37,7 @@
 		justify-self: center;
 		background-color: #262a34;
 		padding: rem(12);
-        border-radius: rem(20);
+		border-radius: rem(20);
 
 		.graph-title {
 			color: #5e6272;
---
+++ frontend/src/routes/(private)/tableau-de-bord/Bar.svelte
@@ -0,0 +1,101 @@
+<script lang="ts">
+	import { Chart } from 'chart.js/auto';
+
+	export let dataSet: {
+		libelle: string;
+		valeur: string[];
+	}[] = [];
+
+	const dates = dataSet.find((data) => data.libelle === 'Dates')?.valeur ?? [];
+	const listeningComprehensionData = dataSet.find((data) => data.libelle === 'LC')?.valeur ?? [];
+	const oralComprehensionData = dataSet.find((data) => data.libelle === 'OC')?.valeur ?? [];
+	const writtenExpressionData = dataSet.find((data) => data.libelle === 'WE')?.valeur ?? [];
+
+	let dataChart = {
+		labels: dates,
+		datasets: [
+			{
+				label: 'Compréhension écrite',
+				data: listeningComprehensionData,
+				borderWidth: 0,
+				backgroundColor: '#FBA3FF',
+				borderRadius: 2
+			},
+			{
+				label: 'Compréhension orale',
+				data: oralComprehensionData,
+				borderWidth: 0,
+				backgroundColor: '#7269E3',
+				borderRadius: 2
+			},
+			{
+				label: 'Expression écrite',
+				data: writtenExpressionData,
+				borderWidth: 0,
+				backgroundColor: '#A06AF9',
+				borderRadius: 2
+			}
+		]
+	};
+
+	let chartInstance: Chart<'bar', number[] | string[]>;
+
+	function myChart(element: HTMLCanvasElement) {
+		chartInstance = new Chart<'bar', number[] | string[]>(element, {
+			type: 'bar',
+			data: dataChart,
+			options: {
+				responsive: true,
+				resizeDelay: 200,
+				maintainAspectRatio: false,
+				plugins: {
+					legend: {
+						position: 'bottom',
+						display: true
+					},
+					tooltip: {
+						callbacks: {
+							label: function (context) {
+								const dataset = context.dataset;
+								const value = dataset.data[context.dataIndex] ?? 0;
+								const totalOfDay =
+									Number(listeningComprehensionData[context.dataIndex]) +
+									Number(oralComprehensionData[context.dataIndex]) +
+									Number(writtenExpressionData[context.dataIndex]);
+								let ratio = 0;
+								if (totalOfDay !== 0) {
+									ratio = +value / totalOfDay;
+								}
+								return `${context.dataset.data[context.dataIndex]} (${(ratio * 100).toFixed(2)} %)`;
+							},
+							title: function (context) {
+								return context[0].dataset.label;
+							}
+						}
+					}
+				}
+			}
+		});
+		return {
+			destroy: () => chartInstance.destroy()
+		};
+	}
+</script>
+
+<div class="bar">
+	<canvas use:myChart></canvas>
+</div>
+
+<style lang="scss">
+	.bar {
+		border: none;
+		max-width: rem(1200);
+		min-width: rem(400);
+		min-height: rem(350);
+		width: 100%;
+		@include min-width(desktop) {
+			justify-content: center;
+			padding: 0 rem(72);
+		}
+	}
+</style>
