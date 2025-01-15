35e6805da65c6712fdad60ccce55581b8486b5fd
Hugo <hugopalazzi75@gmail.com>
2025-01-15 22:30:38 +0100

    Feat(Sign up) : user can sign up with email

--- frontend/src/routes/(public)/inscription/+page.server.ts
+++ frontend/src/routes/(public)/inscription/+page.server.ts
@@ -1,23 +1,36 @@
 import { userCreationAccountSchema } from '@tcf/models/forms/userSchema';
 import { zod } from 'sveltekit-superforms/adapters';
 import { superValidate } from 'sveltekit-superforms/client';
+import { message } from 'sveltekit-superforms';
+import { fail } from '@sveltejs/kit';
 
 export const load = async () => {
 	const form = await superValidate(zod(userCreationAccountSchema));
-
 	return {
 		form
 	};
 };
+
 export const actions = {
 	default: async ({ request, locals }) => {
 		const formData = await request.formData();
-		const providentForm = await superValidate(formData, zod(userCreationAccountSchema));
+
+		const form = await superValidate(formData, zod(userCreationAccountSchema));
+
+		if (!form.valid) {
+			return fail(400, { form });
+		}
+
 		await locals.supabase.auth.signUp({
-			email: providentForm.data.email,
-			password: providentForm.data.password
+			email: form.data.email,
+			password: form.data.password,
+			options: {
+				data: {
+					name: form.data.username
+				}
+			}
 		});
 
-		return providentForm;
+		return message(form, 'Création du compte réussis');
 	}
 };
