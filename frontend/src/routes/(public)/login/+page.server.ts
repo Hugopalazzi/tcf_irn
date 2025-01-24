3fd56cfef63a5f8d477a3cdd2bd10eaf8f15e4b5
Hugo <hugopalazzi75@gmail.com>
2025-01-24 17:20:46 +0100

    Feat(Services) : sign up

--- frontend/src/routes/(public)/login/+page.server.ts
+++ frontend/src/routes/(public)/login/+page.server.ts
@@ -1,7 +1,7 @@
 import { userLoginSchema } from '@tcf/models/forms/userSchema';
 import { UserService } from '@tcf/services/supabase/user.service.js';
 import { zod } from 'sveltekit-superforms/adapters';
-import { fail, superValidate } from 'sveltekit-superforms/client';
+import { superValidate } from 'sveltekit-superforms/client';
 
 export const load = async () => {
 	const form = await superValidate(zod(userLoginSchema));
@@ -15,11 +15,6 @@ export const actions = {
 	default: async ({ request, locals }) => {
 		const formData = await request.formData();
 		const form = await superValidate(formData, zod(userLoginSchema));
-
-		if (!form.valid) {
-			return fail(400, { form });
-		}
-
 		const userService = new UserService(locals.supabase);
 		return await userService.login(form);
 	}
