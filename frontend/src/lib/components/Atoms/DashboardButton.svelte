0739870f2d7f39b7be4fa2fdddee4aa59a6bebcb
Hugo <hugopalazzi75@gmail.com>
2025-04-28 22:38:45 +0200

    active link when it shoul

---
+++ frontend/src/lib/components/Atoms/DashboardButton.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import '@tcf/styles/styles.scss';
+	import type { Snippet } from 'svelte';
+
+	interface Props {
+		href: string;
+		children: Snippet;
+	}
+
+	let { href = '', children }: Props = $props();
+</script>
+
+<a {href} class="btn btn-primary" class:active={page.url.pathname.startsWith(href)}>
+	{@render children()}
+</a>
+
+<style lang="scss">
+	.active {
+		background-color: var(--secondary-color);
+		:global(span) {
+			color: black;
+		}
+	}
+</style>
