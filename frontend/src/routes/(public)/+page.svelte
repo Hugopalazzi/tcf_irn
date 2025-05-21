e759cce57eb9e6ea894404ff8605645ef31fc0c2
Hugo <hugopalazzi75@gmail.com>
2025-05-21 14:00:00 +0200

    Feat(Home): Delete home page and replace with 2 buttons only

--- frontend/src/routes/(public)/+page.svelte
+++ frontend/src/routes/(public)/+page.svelte
@@ -1,35 +1,21 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import { Envelope, Icon } from 'svelte-hero-icons';
-	import Multiselect from './Multiselect.svelte';
-	import GoogleIcon from '@tcf/lib/components/Icons/GoogleIcon.svelte';
+	import Button from '@tcf/lib/components/Atoms/Button.svelte';
 </script>
 
-<div class="page-container">
-	<div class="left-size">
-		<img class="logo" src="/assets/images/logo_my_story.png" alt="My story" />
-		<h5>Apprenez le francais en vous amusant avec Mystory.</h5>
-	</div>
-	<div class="right-size">
-		<Multiselect />
-		<button
-			onclick={() => {
-				goto('/inscription');
-			}}
-			class="btn btn-primary btn-icon">
-			<Icon src={Envelope} size="18" />Create an account
-		</button>
-
-		<div class="two-button">
-			<a class="btn btn-secondary" href="/auth/login/google"><GoogleIcon /></a>
-
-			<button
-				onclick={() => {
-					goto('/login');
-				}}
-				class="btn btn-secondary">
-				<span>Login</span>
-			</button>
-		</div>
-	</div>
+<div>
+	<Button
+		onClick={() => {
+			goto('/registration');
+		}}
+		extraClass="centered-submit-button"
+		color="primary"
+		label="Se créer un compte"></Button>
+	<Button
+		onClick={() => {
+			goto('/login');
+		}}
+		extraClass="centered-submit-button"
+		color="secondary"
+		label="Se connecter"></Button>
 </div>
