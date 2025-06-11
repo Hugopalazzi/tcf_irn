f45c8a4b92e19384ef1b29149059620e787f939f
Hugo <hugopalazzi75@gmail.com>
2025-06-11 21:43:36 +0200

    Fix(ProgressBar): responsive

--- frontend/src/lib/components/Organisms/ExamTopCard.svelte
+++ frontend/src/lib/components/Organisms/ExamTopCard.svelte
@@ -13,19 +13,55 @@
 
 <div class={bem('container')}>
 	<span class={bem('question-number')}>Question 1</span>
-	<ProgressBar progress={12} />
+	<div class={bem('progress-bar-wrapper')}>
+		<ProgressBar progress={12} largeProgressBar={true} />
+	</div>
+	<span class={bem('percentage')}>10%</span>
 </div>
 
 <style lang="scss">
 	.exam-top-card {
 		&__container {
-			display: flex;
+			display: grid;
+			grid-template-columns: repeat(2, 1fr);
+			grid-template-rows: repeat(2, 1fr);
+			grid-column-gap: 0px;
+			grid-row-gap: 0px;
+		}
+
+		&__question-number,
+		&__percentage {
+			font-weight: 400;
+			color: rgba(0, 0, 0, 0.5);
+			white-space: nowrap;
+			font-size: rem(14);
 		}
 
 		&__question-number {
-			font-weight: 400;
-			color: rgba(0, 0, 0, 0.5);
-			font-size: rem(14);
+			grid-area: left;
+			justify-self: start;
+			grid-area: 2 / 1 / 3 / 2;
+		}
+
+		&__progress-bar-wrapper {
+			grid-area: 1 / 1 / 2 / 3;
+		}
+
+		&__percentage {
+			grid-area: right;
+			justify-self: end;
+			grid-area: 2 / 2 / 3 / 3;
+		}
+	}
+
+	@media (min-width: $breakpoint-tablet) {
+		.exam-top-card {
+			&__container {
+				display: flex;
+				flex-direction: row;
+				align-items: center;
+				gap: 20px;
+			}
 		}
 	}
 </style>
