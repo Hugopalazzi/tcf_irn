3d855e39bc434b68820cd1c8d3fc934f8436de33
Hugo <hugopalazzi75@gmail.com>
2025-06-20 21:44:33 +0200

    feat(ScoreCards): create component

--- frontend/src/lib/components/Organisms/ScoreCards.svelte
+++ frontend/src/lib/components/Organisms/ScoreCards.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
-	import ScoreCardElement, { type ScoreCardElementProps } from '@tcf/lib/components/Molecules/ScoreCardElement.svelte';
 	import { createBEM } from '@tcf/lib/helpers/bemHelper';
-	import ExperienceCard from '@tcf/lib/components/Molecules/ExperienceCard.svelte';
-	import VerticalLineIcon from '@tcf/lib/components/Icons/VerticalLineIcon.svelte';
+	import ScoreCardElement, { type ScoreCardElementProps } from '@tcf/lib/components/Molecules/ScoreCardElement.svelte';
 
 	type Props = {
 		scores: ScoreCardElementProps[];
@@ -13,41 +11,16 @@
 	const bem = createBEM('score-cards');
 </script>
 
-<div class={bem('container')}>
+<div class="score-cards-container">
 	{#each scores as score}
 		<ScoreCardElement {...score} />
 	{/each}
-	<div class={bem('vertical-bar')}>
-		<VerticalLineIcon />
-	</div>
-	<ExperienceCard level={1} experience={20} maxExperience={200} />
 </div>
 
 <style lang="scss">
-	.score-cards {
-		&__container {
-			display: grid;
-			grid-template-columns: 1fr 1fr;
-			width: 100%;
-			gap: rem(12) rem(8);
-		}
-
-		&__vertical-bar {
-			display: none;
-		}
-	}
-
-	@media (min-width: $breakpoint-desktop) {
-		.score-cards {
-			&__container {
-				display: flex;
-				gap: rem(20);
-				align-items: center;
-			}
-
-			&__vertical-bar {
-				display: block;
-			}
-		}
+	.score-cards-container {
+		display: flex;
+		gap: rem(20);
+		width: 100%;
 	}
 </style>
