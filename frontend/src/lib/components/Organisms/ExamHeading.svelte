4e338d7133cb67ac300db6901036d9f078d1ae46
Leo Palazzi <leopalazzi@outlook.fr>
2025-06-23 21:40:58 +0200

    Bugfix Progress Bar : Update bar with current question index

--- frontend/src/lib/components/Organisms/ExamHeading.svelte
+++ frontend/src/lib/components/Organisms/ExamHeading.svelte
@@ -20,7 +20,9 @@
 	<div class={bem('content')}>
 		<span class={bem('question-number')}>{t('examHeading.questionNumber', { number: currentQuestionIndex + 1 })}</span>
 		<div class={bem('progress-bar-wrapper')}>
-			<ProgressBar progress={progressPercentage} />
+			{#key currentQuestionIndex}
+				<ProgressBar progress={progressPercentage} />
+			{/key}
 		</div>
 		<span class={bem('percentage')}>{t('examHeading.questionPercentage', { percent: progressPercentage })}</span>
 	</div>
