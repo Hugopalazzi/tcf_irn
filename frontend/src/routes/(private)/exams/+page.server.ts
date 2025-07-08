f982532521c55b411d1499788b54ca974974023a
Leo Palazzi <leopalazzi@outlook.fr>
2025-07-08 22:32:13 +0200

    Feat: Exam History | Update mappers

--- frontend/src/routes/(private)/exams/+page.server.ts
+++ frontend/src/routes/(private)/exams/+page.server.ts
@@ -1,11 +1,11 @@
-import { buildExamsHistory } from "@tcf/lib/helpers/examsHistoryHelper";
+import { generateExamsHistory } from "@tcf/lib/mappers/examsHistoryMapper";
 
 export const load = async ({ locals: { supabase } }) => {
     const { data: userExamsHistoryData, error: fetchError } = await supabase.from('user_exams').select();
     if (fetchError) {
         throw new Error('Failed to fetch exam history: ' + fetchError.message);
     }
-    const userExamsHistory = buildExamsHistory(userExamsHistoryData);
+    const userExamsHistory = generateExamsHistory(userExamsHistoryData);
 
-    return {userExamsHistory};
+    return { userExamsHistory };
 };
