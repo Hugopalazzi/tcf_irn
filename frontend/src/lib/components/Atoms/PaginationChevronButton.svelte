1b762dcf04156bb6a91107aef2754443e31343a8
Hugo <hugopalazzi75@gmail.com>
2025-07-10 22:57:37 +0200

    Feat(Pagination) : Chevron component

---
+++ frontend/src/lib/components/Atoms/PaginationChevronButton.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { createBEM } from '@tcf/lib/helpers/bemHelper';
+	import ChevronLeftIcon from '@tcf/lib/components/Icons/ChevronLeftIcon.svelte';
+	import ChevronRightIcon from '@tcf/lib/components/Icons/ChevronRightIcon.svelte';
+
+	type Props = {
+		onClick: () => void;
+		chevronDirection: 'left' | 'right';
+		disabled?: boolean;
+	};
+
+	const { onClick, chevronDirection, disabled }: Props = $props();
+
+	const onKeyDown = (event: KeyboardEvent) => {
+		if (event.code === 'Space') {
+			event.preventDefault();
+		}
+	};
+	const bem = createBEM('pagination-chevron-button');
+</script>
+
+<button type="button" class={bem('')} role="link" onclick={onClick} onkeydown={onKeyDown} {disabled}>
+	{#if chevronDirection === 'left'}
+		<ChevronLeftIcon color={disabled ? '#ACACAC' : '#111827'} />
+	{:else}
+		<ChevronRightIcon color={disabled ? '#ACACAC' : '#111827'} />
+	{/if}
+</button>
+
+<style lang="scss">
+	.pagination-chevron-button {
+		display: flex;
+		padding: rem(8);
+		border-radius: rem(8);
+		border: 1px solid #dcdcdc;
+		background-color: #f9f9f9;
+
+		&:disabled {
+			background-color: #f1f1f1;
+			color: gray;
+		}
+	}
+</style>
---
+++ frontend/src/lib/components/Icons/ChevronLeftIcon.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	interface Props {
+		color?: string;
+	}
+
+	const { color = '#ACACAC' }: Props = $props();
+</script>
+
+<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+	<path
+		fill-rule="evenodd"
+		clip-rule="evenodd"
+		d="M10.4716 3.52858C10.7319 3.78892 10.7319 4.21103 10.4716 4.47138L6.94297 7.99998L10.4716 11.5286C10.7319 11.7889 10.7319 12.211 10.4716 12.4714C10.2112 12.7317 9.78911 12.7317 9.52876 12.4714L5.52876 8.47138C5.26841 8.21103 5.26841 7.78892 5.52876 7.52858L9.52876 3.52858C9.78911 3.26823 10.2112 3.26823 10.4716 3.52858Z"
+		fill={color} />
+</svg>
